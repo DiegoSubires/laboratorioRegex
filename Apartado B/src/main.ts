3c3a9b9aeae281a25a1c2eb7be05918d08829a31
Diego Subires <diegosubires@gmail.com>
2024-10-23 15:14:38 +0200

    Subir a repositorio

--- Apartado B/src/main.ts
+++ Apartado B/src/main.ts
@@ -1,22 +1,68 @@
 import "./style.css";
-
-export interface CrearBotonParams {
-  texto: string;
-  id: string;
-  nombreClase: string;
-  onClick: (id: string) => void;
-}
+// import { pintarPersonajes } from "./personajes-listado";
 
 export let HTMLtext: string = "";
 export const patron =
   /(?<prefijo><img\ssrc=")(?<enlace>.{0,})(?<sufijo>"\s\/>)$/gm;
 export const inputHTML = document.getElementById("inputHTML");
 export const extraer = document.getElementById("extraer");
+export const borrar = document.getElementById("borrar");
+// export const convertir = document.getElementById("convertir");
+
+export function habilitarBotones(
+  nombreBoton: HTMLElement | null,
+  ishabilitado: boolean
+) {
+  if (nombreBoton && nombreBoton instanceof HTMLButtonElement) {
+    ishabilitado
+      ? (nombreBoton.disabled = false)
+      : (nombreBoton.disabled = true);
+  }
+}
 
 export const crearElementoParrafo = (texto: string): HTMLParagraphElement => {
+  const div = document.getElementById("contenedor");
+
   const parrafo = document.createElement("p");
+  div?.appendChild(parrafo);
   parrafo.textContent = texto;
   return parrafo;
+};
+
+export const crearElementoBoton = (
+  texto: string,
+  id: string
+): HTMLButtonElement => {
+  const div = document.getElementById("contenedor");
+
+  const boton = document.createElement("button");
+  div?.prepend(boton);
+  boton.id = id;
+  boton.classList.add("boton");
+  boton.textContent = texto;
+  return boton;
+};
+export const crearContenedorImagenes = (): HTMLDivElement => {
+  const div = document.getElementById("contenedor");
+
+  const contenedorImagenes = document.createElement("div");
+  div?.appendChild(contenedorImagenes);
+  contenedorImagenes.id = "listado-personajes";
+  contenedorImagenes.classList.add("listado-personajes");
+
+  return contenedorImagenes;
+};
+
+export const crearElementoImagen = (texto: string): HTMLImageElement => {
+  const div = document.getElementById("contenedorImagenes");
+  const contenedorImagen = document.createElement("div");
+  div?.appendChild(contenedorImagen);
+
+  const imagen = document.createElement("img");
+  contenedorImagen?.appendChild(imagen);
+  imagen.src = texto;
+  imagen.title = texto;
+  return imagen;
 };
 
 export const eliminarElementos = (str: string) => {
@@ -37,33 +83,90 @@ export function obtenerHTMLText() {
   return HTMLtext;
 }
 
-export const extraerSrc = (str: string) => {
-  const coincidencia = patron.exec(str);
-  if (coincidencia) {
-    const { enlace } = coincidencia.groups as any;
-    return enlace;
+export const extraerEnlacesImagenes = (value: string) => {
+  const arr = [...value.matchAll(patron)];
+  const enlaces = arr.map((arr) => arr.groups?.enlace);
+  return enlaces;
+};
+export const pintarResultado = (strArr: (string | undefined)[]) => {
+  if (strArr !== undefined) {
+    strArr.map((el) => {
+      if (el !== undefined) {
+        crearElementoParrafo(el);
+      }
+    });
+  }
+};
+export const pintarImagenes = (strArr: (string | undefined)[]) => {
+  if (strArr !== undefined) {
+    const listado = document.querySelector("#listado-personajes");
+    strArr.map((el) => {
+      if (el !== undefined && listado && listado instanceof HTMLDivElement) {
+        const contenedorPersonaje = crearContenedorPersonaje(el);
+        listado.appendChild(contenedorPersonaje);
+      }
+    });
   }
 };
 
-export const extratoArray = (value: string) => value.match(patron);
+export const crearContenedorPersonaje = (personaje: string): HTMLDivElement => {
+  const elementoPersonaje = document.createElement("div");
+  elementoPersonaje.classList.add("personaje-contenedor");
+  const imagen = crearElementoImagen(personaje);
+  elementoPersonaje.appendChild(imagen);
+  return elementoPersonaje;
+};
 
-export const extraerSRC = (extractoArray: RegExpMatchArray | null) => {
-  if (extractoArray !== null) {
-    let extractoSRC: string[] = [];
-    extractoSRC = extractoArray.map((el) => extraerSrc(el));
-    return extractoSRC;
+export const pintarPersonajes = (str: string[]): void => {
+  const listado = document.querySelector("#listado-personajes");
+  if (listado && listado instanceof HTMLDivElement) {
+    str.forEach((personaje) => {
+      const contenedorPersonaje = crearContenedorPersonaje(personaje);
+      listado.appendChild(contenedorPersonaje);
+    });
   } else {
-    return null;
+    throw new Error("No se ha encontrado el contenedor del listado");
   }
 };
 
-export function handleClick() {
+export function handleClickExtraer() {
   obtenerHTMLText();
   console.log(HTMLtext);
-  console.log(extratoArray(HTMLtext));
-  console.log(extraerSRC(extratoArray(HTMLtext)));
+  console.log(extraerEnlacesImagenes(HTMLtext));
+
+  if (extraerEnlacesImagenes(HTMLtext).length === 0) {
+    crearElementoParrafo("No hay enlaces a imágenes válidos en el texto");
+  } else {
+    pintarResultado(extraerEnlacesImagenes(HTMLtext));
+    crearElementoBoton("CONVERTIR A IMÁGENES", "convertir");
+    crearContenedorImagenes();
+    const convertir = document.getElementById("convertir");
+    if (convertir && convertir instanceof HTMLButtonElement) {
+      convertir.addEventListener("click", handleClickConvertir);
+    }
+  }
+  habilitarBotones(extraer, false);
+  habilitarBotones(borrar, true);
+}
+
+export function handleClickBorrar() {
+  if (inputHTML && inputHTML instanceof HTMLTextAreaElement) {
+    inputHTML.value = "";
+  }
+  HTMLtext = "";
+  eliminarElementos("contenedor");
+  habilitarBotones(extraer, true);
+  habilitarBotones(borrar, false);
+  console.log(HTMLtext);
+}
+
+export function handleClickConvertir() {
+  pintarImagenes(extraerEnlacesImagenes(HTMLtext));
 }
 
 if (extraer && extraer instanceof HTMLButtonElement) {
-  extraer.addEventListener("click", handleClick);
+  extraer.addEventListener("click", handleClickExtraer);
+}
+if (borrar && borrar instanceof HTMLButtonElement) {
+  borrar.addEventListener("click", handleClickBorrar);
 }
